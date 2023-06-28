f3e305fbea33fd8456edbd001342f02586df04f3
Paula <58055615+apaulag@users.noreply.github.com>
2023-06-28 16:36:13 +0000

    Update 2 files

--- exercise.js
+++ exercise.js
@@ -1,8 +1,24 @@
-// Your code goes here
+const getItemById = (items, id) => items.filter((item) => item.id === id)[0];
+
+const buildTransactions = (sales, items) => {
+  return sales.map((sale) => {
+    const { description, price } = getItemById(items, sale.itemId);
+    return {
+      ...sale,
+      description,
+      price,
+    };
+  });
+};
+
+const getTransactionsByItemDescription = (transactions, description) => {
+  return transactions.filter(
+    (transaction) => transaction.description === description
+  );
+};
 
 module.exports = {
-    // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
-}
+  getItemById,
+  buildTransactions,
+  getTransactionsByItemDescription,
+};
